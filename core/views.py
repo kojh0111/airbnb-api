4e80c111da2e6094f362d9878cff805748eb4f96
jh <kojh0111@naver.com>
2021-11-01 11:48:46 +0900

    APIs in Django Way

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse
+from rooms.models import Room
+
 
 # Create your views here.
+def list_rooms(request):
+    rooms = Room.objects.all()
+    data = serializers.serialize("json", rooms)
+    response = HttpResponse(data)
+    return response
