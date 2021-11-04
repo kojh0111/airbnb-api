d2ff2172da7b3c5a0fd3e23cdd3d9d584e5f639c
jh <kojh0111@naver.com>
2021-11-04 10:27:54 +0900

    Build REST API(Create Room)

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ListRoomsView.as_view()),
+    path("", views.rooms_view),
     path("<int:pk>/", views.SeeRoomView.as_view()),
 ]
