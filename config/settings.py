2e3a498b506de76d3d95a08f214316ffb296f9d9
jh <kojh0111@naver.com>
2021-11-07 20:20:26 +0900

    Log in - JWT(2)

--- config/settings.py
+++ config/settings.py
@@ -139,4 +139,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "config.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
 }
