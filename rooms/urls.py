a484048b9f934a47ac2bf4d58a6c2a441a730609
jh <kojh0111@naver.com>
2021-11-09 17:58:36 +0900

    RoomViewSet Permissions

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
-urlpatterns = [
-    path("", views.RoomsView.as_view()),
-    path("search/", views.room_search),
-    path("<int:pk>/", views.RoomView.as_view()),
-]
+app_name = "rooms"
+router = DefaultRouter()
+router.register("", views.RoomViewSet)
+
+urlpatterns = router.urls
