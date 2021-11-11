cc641c05a2cd40def9054bb14c7ee4a305b8f469
jh <kojh0111@naver.com>
2021-11-11 16:36:50 +0900

    Including login in Viewset

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser, AllowAny
@@ -26,8 +26,23 @@ class UserViewSet(ModelViewSet):
         elif self.action == "create" or self.action == "retrieve":
             permission_classes = [AllowAny]
         else:
-            permission_classes = [IsSelf | IsAdminUser]
+            permission_classes = [IsSelf]
         return [permission() for permission in permission_classes]
+
+    @action(detail=False, methods=["POST"])
+    def login(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            encoded_jwt = jwt.encode(
+                {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
+            )
+            return Response(data={"token": encoded_jwt})
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
 class MeView(APIView):
@@ -69,28 +84,3 @@ class FavsView(APIView):
             except Room.DoesNotExist:
                 pass
         return Response(status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["GET"])
-def user_detail(request, pk):
-    try:
-        user = User.objects.get(pk=pk)
-        return Response(UserSerializer(user).data)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-@api_view(["POST"])
-def login(request):
-    username = request.data.get("username")
-    password = request.data.get("password")
-    if not username or not password:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        encoded_jwt = jwt.encode(
-            {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
-        )
-        return Response(data={"token": encoded_jwt})
-    else:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
